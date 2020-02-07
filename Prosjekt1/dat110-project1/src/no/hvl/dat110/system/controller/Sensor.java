04bfb668d96d652944e3c68fd9f27f396f95cba0
579683 <42578182+579683@users.noreply.github.com>
2020-02-07 13:22:42 +0100

    Add files via upload

---
+++ Prosjekt1/dat110-project1/src/no/hvl/dat110/system/controller/Sensor.java
@@ -0,0 +1,24 @@
+package no.hvl.dat110.system.controller;
+
+import no.hvl.dat110.TODO;
+import no.hvl.dat110.rpc.*;
+
+public class Sensor extends RPCStub {
+
+	private byte RPCID = 1;
+
+	public int read() {
+		int temp;
+
+		byte[] request = RPCUtils.marshallVoid(RPCID);
+
+		byte[] reply = rpcclient.call(request);
+
+		temp = RPCUtils.unmarshallInteger(reply);
+
+		// TODO
+		// implement marshalling, call and unmarshalling for read RPC method
+		return temp;
+	}
+
+}
